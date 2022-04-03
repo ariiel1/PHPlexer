29883d69a5bac33d371f047311a1739459c3407a
ariel <71622992+ariiel1@users.noreply.github.com>
2022-04-04 03:24:16 +0700

    Update code.php

--- code.php
+++ code.php
@@ -1,6 +1,13 @@
 <?php
 class MyClass {
     function abc(){ $i=5;
+    #comment
+    //another comment
+    /*
+        yet another comment    
+        $z=$i*2; 
+    asd
+    */
     $z=$i*2;
     echo "One '$=".$z;}
 }
